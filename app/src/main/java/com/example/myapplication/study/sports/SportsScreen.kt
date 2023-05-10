1af2ce160055597f0dbbd94df01c0fe1eb1dbfa1
내쉬_송종혁 <samohao@afreecatv.com>
2023-05-10 09:15:00 +0900

    add sprots

--- app/src/main/java/com/example/myapplication/study/sports/SportsScreen.kt
+++ app/src/main/java/com/example/myapplication/study/sports/SportsScreen.kt
@@ -3,17 +3,21 @@ package com.example.myapplication.study.sports
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.study.sports.presenter.SportsViewModel
+import com.example.myapplication.study.sports.presenter.StateSport
+import com.example.myapplication.study.sports.theme.Pink80
 import com.example.myapplication.study.sports.theme.SportsTheme
 
 @Composable
@@ -25,26 +29,78 @@ fun SportScreenView(
     val backHandlingEnable by remember { mutableStateOf(false) }
     BackHandler(backHandlingEnable, onBack = backPress)
 
+    val state = sportsViewModel.stateSport.collectAsState().value
+
     Scaffold(topBar = {
-        Text(text = "Sports")
+        SportTop(state = state, windowSize = windowSize)
     }) { padding ->
         SportsTheme {
-            SportsMain(windowSize, padding)
+            SportsMain(
+                windowSize = windowSize,
+                padding = padding,
+                sportsViewModel = sportsViewModel
+            )
         }
+    }
+}
+
+@Composable
+fun SportTop(state: StateSport, windowSize: WindowSizeClass) {
+    var hasBack = false
+    var title = "LIST"
+    when (state) {
+        is StateSport.Detail -> {
+            hasBack = true
+            title = "DETAIL"
+
+        }
+        else -> {}
+    }
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Expanded -> {
+            hasBack = true
+            title = "DETAIL"
+        }
+        else -> {}
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(horizontal = 50.dp)
+            .background(color = Pink80),
+        horizontalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        if (hasBack) Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+        Text(text = title)
     }
 }
 
 @Composable
 fun SportsMain(
     windowSize: WindowSizeClass,
-    padding: PaddingValues
+    padding: PaddingValues,
+    sportsViewModel: SportsViewModel
 ) {
+
+//    when (state) {
+//        is StateSport.Start -> {
+//
+//        }
+//        is StateSport.List -> {
+//
+//        }
+//        is StateSport.Detail -> {
+//
+//        }
+//    }
+
     when (windowSize.widthSizeClass) {
         WindowWidthSizeClass.Compact -> {
-            SportsList()
+            SportsList(padding = padding)
         }
         WindowWidthSizeClass.Medium -> {
-            SportsList()
+            SportsList(padding = padding)
         }
         WindowWidthSizeClass.Expanded -> {
             SportsListAndDetail(padding)
@@ -53,14 +109,25 @@ fun SportsMain(
 }
 
 @Composable
-fun SportsList(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxWidth().background(color = Color.Red)) {
+fun SportsList(modifier: Modifier = Modifier, padding: PaddingValues) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color.Red)
+            .padding(padding)
+    ) {
         Text(text = "List")
     }
 }
+
 @Composable
-fun SportsDetail(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxWidth().background(color = Color.Black)) {
+fun SportsDetail(modifier: Modifier = Modifier, padding: PaddingValues) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color.Blue)
+            .padding(padding)
+    ) {
         Text(text = "Detail")
     }
 }
@@ -71,7 +138,7 @@ fun SportsListAndDetail(padding: PaddingValues) {
         val weight = Modifier
             .weight(1f)
             .padding(padding)
-        SportsList(weight)
-        SportsDetail(weight)
+        SportsList(weight, padding = padding)
+        SportsDetail(weight, padding = padding)
     }
 }
--- app/src/main/java/com/example/myapplication/study/sports/presenter/SportsViewModel.kt
+++ app/src/main/java/com/example/myapplication/study/sports/presenter/SportsViewModel.kt
@@ -1,7 +1,93 @@
 package com.example.myapplication.study.sports.presenter
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myapplication.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class SportsViewModel: ViewModel() {
+class SportsViewModel : ViewModel() {
+    private val _sportState = MutableStateFlow<StateSport>(StateSport.Start)
+    val stateSport = _sportState.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = StateSport.Start
+    )
+    fun getSportList() = listSport
 
+    fun eventHandler(event: EventSport) {
+        viewModelScope.launch {
+            when (event) {
+                is EventSport.ClickSport -> {
+                    _sportState.emit(StateSport.Detail(item = event.item))
+                }
+                is EventSport.ClickNavBack -> {
+
+                }
+            }
+        }
+    }
 }
+
+sealed interface EventSport {
+    data class ClickSport(val item: SportItem): EventSport
+    object ClickNavBack: EventSport
+}
+
+sealed interface StateSport {
+    object Start: StateSport
+
+    object List: StateSport
+
+    data class Detail(val item: SportItem = SportItem()): StateSport
+}
+
+val listSport = listOf(
+    SportItem(
+        R.drawable.art_image1,
+        name = "baseball",
+        desc = "Here is some Baseball news!",
+        "padding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = padding"
+    ),
+    SportItem(
+        R.drawable.art_image2,
+        name = "Badminton",
+        desc = "Here is some Badminton news!",
+        "padding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = padding"
+    ),
+    SportItem(
+        R.drawable.art_image3,
+        name = "basketball",
+        desc = "Here is some basketball news!",
+        "padding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = padding"
+    ),
+    SportItem(
+        R.drawable.art_image4,
+        name = "bowling",
+        desc = "Here is some bowling news!",
+        "padding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = padding"
+    ),
+    SportItem(
+        R.drawable.art_image5,
+        name = "cycling",
+        desc = "Here is some cycling news!",
+        "padding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = padding"
+    ),
+    SportItem(
+        R.drawable.art_image6,
+        name = "deadlift",
+        desc = "Here is some Baseball news!",
+        "padding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = paddingpadding = padding"
+    ),
+)
+
+
+data class SportItem(
+    val icon: Int = -1,
+    val name: String = "",
+    val desc: String = "",
+    val detail: String = ""
+)
